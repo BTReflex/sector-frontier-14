41b393c5c21cde9a8a4b5ab960596a400ef05df4
Tea_BeTA <BTReflex@yandex.ru>
2026-06-11 16:55:34 +0600

    Ещё чуть более рабочая хрень

--- Content.Client/_Goobstation/Clothing/EntitySystems/HideClothingLayerClothingSystem.cs
+++ Content.Client/_Goobstation/Clothing/EntitySystems/HideClothingLayerClothingSystem.cs
@@ -4,7 +4,7 @@
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
 using Content.Client._Goobstation.Clothing.Components;
-using Content.Server._Goobstation.Clothing.Common;
+using Content.Shared._Goobstation.Clothing.Common;
 using Content.Shared.Inventory;
 using Content.Shared.Inventory.Events;
 using Content.Shared.Item;
